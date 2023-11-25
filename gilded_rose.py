5ea25f17f77d6cda4bccbf65fd6d58030663504d
Jadhav Rohit <jadhavrohit@Jadhavs-MacBook-Air.local>
2023-11-25 14:16:07 +0000

    added function for aged brie condition and function to update sell_in

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,39 +1,24 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
+    MAX_QLTY = 50
+    MIN_QLTY = 0
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Aged Brie":
+                self._update_aged_brie(item)
+
+    def _update_aged_brie(self, item):
+        if item.quality < GildedRose.MAX_QLTY:
+            item.quality += 1
+        self._update_sell_in(item)
+
+    def _update_sell_in(self, item):
+        item.sell_in -= 1
 
 
 class Item:
