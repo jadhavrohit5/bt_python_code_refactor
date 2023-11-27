c086fa78283adab4ff89d185a03ff2e83ea1c61c
Jadhav Rohit <jadhavrohit@Jadhavs-MacBook-Air.local>
2023-11-27 22:02:47 +0000

    added unit test cases for the conjured item

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,24 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+    def test_conjured_item(self):
+        # Test 1: Degrades 2x faster
+        item = Item("Conjured Mana Cake", 10, 20)
+        gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertGreaterEqual(item.quality, 18)
+
+        # Test 2: Degrades 4x faster after sell_in
+        item.sell_in = -10
+        item.quality = 20
+        gilded_rose.update_quality()
+        self.assertEqual(item.quality, 16)
+
+        # Test 3: Never negative
+        item.sell_in = -10
+        item.quality = 1
+        gilded_rose.update_quality()
+        self.assertEqual(item.quality, 0)
 
         
 if __name__ == '__main__':
